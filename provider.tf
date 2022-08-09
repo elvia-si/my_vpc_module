d2fbdf0022f10feb730eaea7555676c17b5f42b3
elvia-si <elvia.sicuro@cloudreach.com>
2022-08-09 18:03:37 +0200

    trying omitting providers

--- provider.tf
+++ provider.tf
@@ -7,12 +7,12 @@
 #   region = "us-east-1"
 # }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.7.0"
-      configuration_aliases = [ aws.east ]
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 2.7.0"
+#       configuration_aliases = [ aws.east ]
+#     }
+#   }
+# }
